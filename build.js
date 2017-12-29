d6c1caf6ca6afd10896f0b863d30715b7f351dda
yuche <i@yuche.me>
2017-12-29 15:06:19 +0800

    Updates

--- build.js
+++ build.js
@@ -1,11 +1,18 @@
-const cp = require('child_process')
-const ghPages = require('gh-pages')
-cp.exec('gitbook build', (err) => {
+const cp = require("child_process");
+const ghPages = require("gh-pages");
+const ora = require("ora");
+const spinner = ora("Publishing gitbooks...").start();
+
+cp.exec("npm run book", err => {
   if (!err) {
-    ghPages.publish('./_book', (err) => {
+    ghPages.publish("./_book", err => {
       if (!err) {
-        console.log('Publish to gh-pages.')
+        spinner.succeed("Publish successfully.");
+      } else {
+        spinner.fail(err);
       }
-    })
+    });
+  } else {
+    spinner.fail(err);
   }
-})
+});
